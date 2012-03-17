952122dcfdd2074e2f5bd29ee901110534f9bceb
Jeff McGee <JeffAMcGee@gmail.com>
2012-03-17 18:29:26 -0500

    attempting to make distutils package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='maroon',
+    version='0.1',
+    url = "http://github.com/jeffamcgee/maroon/",
+    packages=['maroon',],
+    install_requires=['pymongo',]
+)
